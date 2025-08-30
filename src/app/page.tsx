80f79ec17d12a84fb9b47be0a713f7c1b7f54b52
Giared Mati <giared.mati@gmail.com>
2025-08-30 04:14:45 -0600

    tons of styling udpates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,59 +45,82 @@ export default function Home() {
         <div className={styles.heroText}>
           <span>How Much Did You Spend</span>
           <span> On </span>
-          <span style={{ color: 'lime' }}>Federal Government</span>
+          <span style={{ color: 'lime' }} className={styles.animatedWords}>
+            Federal Government
+          </span>
           <span> programs in 2024?</span>
         </div>
+        <div className={styles.inputBar}>
+          {/* <input
+            type="number"
+            placeholder="Enter your income here"
+            value={income || ''}
+            onChange={e => setIncome(parseFloat(e.target.value) || 0)}
+            className={styles.incomeInput}
+          /> */}
+        </div>
         <div className={styles.main}>
-          <div className={styles.inputContainer}>
-            <div className={styles.inputLabels}>
-              <div className={styles.leftLabel}>
-                <p>Enter your yearly income</p>
+          <div className={styles.budgetBarContainer}>
+            <div className={styles.leftLabel}>
+              <p>Start Here</p>
+            </div>
+            <div className={styles.budgetBar}>
+              <p>choose a budget category</p>
+              {budgetOptions.map(b => (
+                <button key={b.value} onClick={() => setBudget(b.value as keyof typeof budgets)}>
+                  {b.label}
+                </button>
+              ))}
+            </div>
+          </div>
+          <div className={styles.heroContainers}>
+            <div className={styles.inputContainer}>
+              <div className={styles.inputLabels}>
                 <input
                   type="number"
-                  placeholder="Enter your income here"
                   value={income || ''}
                   onChange={e => setIncome(parseFloat(e.target.value) || 0)}
-                  style={{ width: '100%', height: '40px', marginTop: '20px', marginBottom: '20px' }}
+                  style={{ width: '100%', height: '70px', marginTop: '20px', marginBottom: '20px' }}
                 />
-              </div>
+                <label className={styles.label}>Enter your yearly income</label>
 
-              <div className={styles.rightLabel}>
-                <p>Enter your filing status</p>
-                <select
-                  value={filingStatus}
-                  onChange={e => setFilingStatus(e.target.value as FilingStatus)}
-                  style={{ width: '100%', height: '40px', marginTop: '20px', marginBottom: '20px' }}
-                >
-                  <option value="single">Single</option>
-                  <option value="married-jointly">Married Filing Jointly</option>
-                  <option value="head-of-household">Head of Household</option>
-                </select>
+                <div className={styles.rightLabel}>
+                  <select
+                    value={filingStatus}
+                    onChange={e => setFilingStatus(e.target.value as FilingStatus)}
+                    style={{ width: '100%', height: '70px', marginTop: '20px', marginBottom: '20px' }}
+                  >
+                    <option value="single">Single</option>
+                    <option value="married-jointly">Married Filing Jointly</option>
+                    <option value="head-of-household">Head of Household</option>
+                  </select>
+                  <label className={styles.label}>Enter your filing status</label>
+                </div>
+                {/* 
+              <p>Pick a budget category</p>
+              <select
+                value={budget}
+                onChange={e => setBudget(e.target.value as keyof typeof budgets)}
+                style={{ width: '100%', height: '40px', marginTop: '20px', marginBottom: '20px' }}
+              >
+                {budgetOptions.map(b => (
+                  <option key={b.value} value={b.value}>
+                    {b.label}
+                  </option>
+                ))}
+              </select> */}
               </div>
             </div>
 
-            <p>Pick a budget category</p>
-            <select
-              value={budget}
-              onChange={e => setBudget(e.target.value as keyof typeof budgets)}
-              style={{ width: '100%', height: '40px', marginTop: '20px', marginBottom: '20px' }}
-            >
-              {budgetOptions.map(b => (
-                <option key={b.value} value={b.value}>
-                  {b.label}
-                </option>
-              ))}
-            </select>
-          </div>
-
-          <div className={styles.resultContainer}>
-            <h1>
-              Your contribution to{' '}
-              {budget ? budgetOptions.find(b => b.value === budget)?.label : 'select a budget'}{' '}
-            </h1>
-            <h3>total tax calculation: {formatCurrencyWithSymbol(calculateTax(income, filingStatus))}</h3>
-            <div className={styles.contributionContainer}>
-              <p className={styles.contribution}>{formatCurrencyWithSymbol(baselineContribution)}</p>
+            <div className={styles.resultContainer}>
+              <h1>
+                Your contribution to{' '}
+                {budget ? budgetOptions.find(b => b.value === budget)?.label : 'select a budget'}{' '}
+              </h1>
+              <h3>total tax calculation: {formatCurrencyWithSymbol(calculateTax(income, filingStatus))}</h3>
+              <div className={styles.contributionContainer}>
+                <p className={styles.contribution}>{formatCurrencyWithSymbol(baselineContribution)}</p>
+              </div>
             </div>
           </div>
         </div>
